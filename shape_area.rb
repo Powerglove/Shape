31642519ecf21c8533d1724848ff637afa4a8cff
mariaalavez <mariaalavez@hotmail.com>
2015-03-11 22:18:08 +0100

    Defined show_area method

--- shape_area.rb
+++ shape_area.rb
@@ -18,8 +18,11 @@ class Shape
 
   def area
     _area = @base * @height
-    puts "The area of your #{self.class.name} is #{_area}"
     _area
+  end
+
+  def show_area
+    puts "The area of your #{self.class.name} is #{area}"
   end
 
 
@@ -37,11 +40,13 @@ class Circle < Shape
     @base = radius
     @height = radius
   end
+
+  def area
+    _area = Math::PI * super
+  end
 end
 
 class Triangle < Shape
-
-  # attr_reader :base, :height
 
   def initialize(base, height)
     super
@@ -51,7 +56,6 @@ class Triangle < Shape
 
   def area
     _area = super / 2
-    puts "The area of your #{self.class.name} is #{_area}"
     _area
   end
 
@@ -60,7 +64,10 @@ end
 
 
 square = Square.new(3)
-# puts square.area
+puts square.show_area
 
 triangle = Triangle.new(4, 5)
-puts triangle.area
+puts triangle.show_area
+
+circle = Circle.new(5)
+puts circle.show_area
