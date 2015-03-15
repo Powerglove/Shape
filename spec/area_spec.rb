47a862234dd8b59cc239431019fe63d0fb863439
mariaalavez <mariaalavez@hotmail.com>
2015-03-15 01:32:54 +0100

    Added attr_reader to child classes

--- spec/area_spec.rb
+++ spec/area_spec.rb
@@ -1,15 +1,10 @@
 
-RSpec.describe "Triangle" do
-
-  before do
-    triangle = Triangle.new(5,4)
-    t_area = triangle.area
-  end
-
-
-    it "has area 10" do
+RSpec.describe "Shape" do
+  
+    it "{Triangle < Shape}" do
+      triangle = Triangle.new(5,4)
+      t_area = triangle.area
       expect(t_area).to eq 10
-
-  end
+    end
 
 end
